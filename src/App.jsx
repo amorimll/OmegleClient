3edc702feb87d8ac5070dcd76b0e3bc431cea98d
amorïm <gabrielmaia.amorim01@gmail.com>
2024-03-27 22:54:31 -0300

    feat: audio included

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   let init = async () => {
     localStream = await navigator.mediaDevices.getUserMedia({
       video: true,
-      audio: false,
+      audio: true,
     });
     document.getElementById("user-1").srcObject = localStream;
 
@@ -44,7 +44,7 @@ function App() {
     if (!localStream) {
       localStream = await navigator.mediaDevices.getUserMedia({
         video: true,
-        audio: false,
+        audio: true,
       });
       document.getElementById("user-1").srcObject = localStream;
     }
